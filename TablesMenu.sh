a8fbee11ceb0ec58abe9c39f45f7d45e50c28864
alaaWalid <alaa2alshitany@gmail.com>
2024-03-08 07:30:08 +0200

    Added select row function

--- TablesMenu.sh
+++ TablesMenu.sh
@@ -142,9 +142,95 @@ selectColumn() {
     done
 }
 
+function show_columns() {
+    tableName=$1
+    metadataFile=".$tableName"
+    
+    if [ -f "$metadataFile" ]; then
+        echo -e "\e[1;35m$(head -n 1 "$metadataFile")\e[0m"
+    else
+        echo "Metadata file for table $tableName does not exist."
+        return 1
+    fi
+}
 
 selectRow() {
-    echo "Selecting a Row"
+    while true
+    do
+    listTables
+        read -p $'\e[1;32mEnter the name of the table you want to list or (exit) to return: \e[0m' selected_table
+        
+        if [[ "$selected_table" == "exit" ]]
+        then
+            echo -e "\e[1;32mReturning to the main menu.\e[0m"
+            break 
+        fi
+
+        if [[ "$selected_table" == "databases" || "$selected_table" == "select" || "$selected_table" == "update" || "$selected_table" == "delete" || "$selected_table" == "insert" || "$selected_table" == "drop" || "$selected_table" == "truncate" ]]
+        then
+            echo -e "\e[1;31mInvalid table name. '$selected_table' is a reserved keyword.\e[0m"
+            continue
+        fi
+
+        if [[ "$selected_table" =~ [[:space:]] ]]
+        then
+            echo -e "\e[1;31mInvalid table name, spaces are not allowed!\e[0m"
+            continue
+        fi
+
+        if [[ ! "$selected_table" =~ ^[a-zA-Z][a-zA-Z0-9_]{0,49}$ ]]
+        then
+            echo -e "\e[1;31mInvalid table name. Please use only alphanumeric characters, underscores, starting with a letter!\e[0m"
+            continue
+        fi
+
+
+        if [ ! -f "${selected_table}" ]
+        then
+            echo -e "\e[1;31mTable '$selected_table' does not exist.\e[0m"
+            continue
+        fi
+        echo "Available columns in table $selected_table:"
+        show_columns "$selected_table"
+
+
+        while true; do
+            read -p "Enter column number to filter by: " columnNumber
+
+            
+            if [[ $columnNumber =~ ^[0-9]+$ ]]; then
+                
+                totalColumns=$(awk -F ':' 'NR==1 {print NF}' ".$selected_table")
+                if ((columnNumber > 0 && columnNumber <= totalColumns)); then
+                    break
+                else
+                    echo "Invalid column number. Please enter a valid column number between 1 and $totalColumns."
+                fi
+            else
+                echo "Invalid input. Please enter a valid number."
+            fi
+        done
+
+        read -p "Enter value to match: " matchingValue
+        matchingRows=$(awk -F ':' -v col="$columnNumber" -v val="$matchingValue" '$col == val {print $0}' "$selected_table")
+
+        if [ -z "$matchingRows" ]; then
+            echo "No matching rows found for the given value."
+            read -p "Do you want to try again? enter y to try again and enter exit to go back to the previous menu: " tryAgainOption
+            if [ "$tryAgainOption" != "y" ]; then
+                return
+            elif [[ "$tryAgainOption" == "exit" ]];then
+                echo -e "\e[1;32mReturning to the main menu.\e[0m"
+                break 2 
+            else
+                echo "Invalid input. Please enter a valid number."
+            fi
+        else
+            echo -e "\e[1;35m$(head -n 1 ".${selected_table}")\e[0m"
+            echo "$matchingRows"
+            break
+        fi
+    done
 }
 
 
